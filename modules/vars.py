8b34fcce5d0ee1b3a0efcb08e5441ca91ad9f38b
teamjaishriram <158039388+teamjaishriram@users.noreply.github.com>
2024-02-02 14:51:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "22609670"
 api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-bot_token = "6876011422:AAEN1M7xRQf-DS8tO_3MGqF1jxVaRncv6yM"
+bot_token = "6949768587:AAE-kq4d4oggNK8zNu91N1fMvFAallstlUw"
