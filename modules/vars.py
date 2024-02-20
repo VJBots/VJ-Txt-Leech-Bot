806f6661de0bc3f6f6fbf7d9a2ecd19bf506ef9d
MatizTechz <154588452+MatizTechz@users.noreply.github.com>
2024-02-20 16:03:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "26373017"
-api_hash = "615a9b06aeb3b518ec2f4c60ce07a635"
-bot_token = "6788468952:AAGYeXCtjtYPRAOV6dL_nCI0pkUpu-kKGNQ"
+api_id = "20493785"
+api_hash = "eabe3e5c86b1d8480fb8ff954837e016"
+bot_token = "7049132082:AAE3tsStWAYFEG-KQqc8vRSiHCTqN61v9UM"
