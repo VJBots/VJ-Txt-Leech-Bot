827594436d53661643bee842555450d827efbf9c
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-29 19:37:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,4 +2,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6627318053:AAEUTVrTA4ldBsMSyKam4zArqXjltTZSwdI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6627318053:AAF6Itu1GUirEACpcwQ5tg9JutasHab0kko")
