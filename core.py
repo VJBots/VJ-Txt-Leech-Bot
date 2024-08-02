b134116dcdd9e9bf40364c91069fd349251bc240
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-08-02 10:10:57 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import os
 import time
 import datetime
