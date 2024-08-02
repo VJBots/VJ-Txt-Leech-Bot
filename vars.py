353e8be77fec91e25c8211a45db41e30fa686fe3
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-08-02 10:13:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from os import environ
 
 API_ID = int(environ.get("API_ID", "22182189"))
