64bf4f43c0eb1b30f3c298ad493d03ab7520b4e7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-11-19 13:25:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
